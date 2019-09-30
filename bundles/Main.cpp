95b2a11881ce6f7bf52e3983247f14d5315b83e9
Simon Jung <simonjung1603@gmail.com>
2019-09-30 03:03:46 +0200

    Test to see if removing the pragmas fixes linux build

--- bundles/Main.cpp
+++ bundles/Main.cpp
@@ -1,9 +1,13 @@
+#ifdef _WIN64
 #pragma warning( push )
 #pragma warning( disable : 4251 )
+#endif
 #include <cppmicroservices/Framework.h>
 #include <cppmicroservices/FrameworkFactory.h>
 #include <cppmicroservices/BundleContext.h>
+#ifdef _WIN64
 #pragma warning( pop )
+#endif
 
 int main(int argc, char* argv[])
 {
